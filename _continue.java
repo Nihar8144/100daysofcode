0e198bf3e7ccbc0898f38ee93a458d42103b9823
Nihar Ranjan Meher <114592364+Nihar8144@users.noreply.github.com>
2022-10-01 00:42:05 +0530

    Add files via upload

---
+++ _continue.java
@@ -0,0 +1,12 @@
+public class _continue {
+    public static void main(String[] args) {
+        for(int i =0;i<10;i++){
+            if(i==5){
+            System.out.println("Break loop");
+            continue;
+            }
+                System.out.println(i);
+                System.out.println("Nihar");
+        }
+    }
+}
---
+++ break_.java
@@ -0,0 +1,15 @@
+public class break_ {
+    public static void main(String[] args) {
+        for(int i=0;i<5;i++){
+            System.out.println(i);
+            System.out.println("JAVA");
+
+            if(i==3){
+                System.out.println("Ending loop");
+                break;
+            }
+        }
+        
+    }   
+    
+}

7a50b461a17375a78b02d76dfff4ea58083d88b9
Nihar Ranjan Meher <114592364+Nihar8144@users.noreply.github.com>
2022-10-06 23:22:31 +0530

    Update table.java

--- Day08/table.java
+++ Day08/table.java
@@ -1,3 +1,7 @@
+/* Topic: Method
+   Question: Print multiplication of a number n using java method */
+
+
 public class table {
     static void multiplication(int x){
         for(int i=1;i<=10;i++){

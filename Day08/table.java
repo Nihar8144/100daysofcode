323c004a9b664424e29369e8e49895e30c04d95f
Nihar Ranjan Meher <114592364+Nihar8144@users.noreply.github.com>
2022-10-06 23:17:56 +0530

    Add files via upload

---
+++ Day08/table.java
@@ -0,0 +1,11 @@
+public class table {
+    static void multiplication(int x){
+        for(int i=1;i<=10;i++){
+            System.out.printf("%d x %d = %d\n",x,i,x*i);
+        }
+    }
+    public static void main(String[] args) {
+        multiplication(4);
+    }
+    
+}

5ef63c181dfb7a7219021abb63ed9040a55ac5bf
Nihar Ranjan Meher <114592364+Nihar8144@users.noreply.github.com>
2022-10-08 01:55:01 +0530

    Add files via upload

---
+++ Day09/method_overloading.java
@@ -0,0 +1,12 @@
+public class method_overloading {
+    static void mov(){
+        System.out.println("Hey Bro!");
+    }
+    static void mov(int a){
+        System.out.println("Hey " + a + " Bro!");
+    }
+    public static void main(String[] args) {
+        mov();
+        mov(3000);
+    }
+}

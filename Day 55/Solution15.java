b29a1abbbbf65e8d5dfd73f530931aea77a0b27e
Nihar Ranjan Meher <114592364+Nihar8144@users.noreply.github.com>
2023-01-25 23:09:04 +0530

    Add files via upload

---
+++ Day 55/Solution15.java
@@ -0,0 +1,27 @@
+/*Sample Input:
+    madam
+Sample Output:
+    Yes
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class Solution15 {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String rev = "";
+        for(int i=A.length()-1;i>=0;i--){
+            rev = rev + A.charAt(i);
+        }
+        if(rev.equals(A)){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+    }
+}

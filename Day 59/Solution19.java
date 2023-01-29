084c62ffa86c2578ccb9d004617a3d69953b5444
Nihar Ranjan Meher <114592364+Nihar8144@users.noreply.github.com>
2023-01-29 16:29:29 +0530

    Add files via upload

---
+++ Day 59/Solution19.java
@@ -0,0 +1,24 @@
+/*Sample Input:
+    13
+Sample Output:
+    prime
+*/
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution19 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String n = bufferedReader.readLine();
+        BigInteger bi = new BigInteger(n);
+        System.out.println(bi.isProbablePrime(10) ? "prime" : "not prime" );
+        bufferedReader.close();
+    }
+}

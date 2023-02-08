b332d52314603a2c281cf2497963d9d377026c30
Nihar Ranjan Meher <114592364+Nihar8144@users.noreply.github.com>
2023-02-08 23:18:45 +0530

    Add files via upload

---
+++ Day 69/Solution29.java
@@ -0,0 +1,40 @@
+/*Output:
+    Generic Sports
+    Each team has n players in Generic Sports
+    Soccer Class
+    Each team has 11 players in Soccer Class
+*/
+
+import java.util.*;
+class Sports{
+
+    String getName(){
+        return "Generic Sports";
+    }
+  
+    void getNumberOfTeamMembers(){
+        System.out.println( "Each team has n players in " + getName() );
+    }
+}
+
+class Soccer extends Sports{
+    @Override
+    String getName(){
+        return "Soccer Class";
+    }
+void getNumberOfTeamMembers () {
+       System.out.println( "Each team has 11 players in " + getName() );
+    }
+}
+
+public class Solution29{
+	
+    public static void main(String []args){
+        Sports c1 = new Sports();
+        Soccer c2 = new Soccer();
+        System.out.println(c1.getName());
+        c1.getNumberOfTeamMembers();
+        System.out.println(c2.getName());
+        c2.getNumberOfTeamMembers();
+	}
+}
